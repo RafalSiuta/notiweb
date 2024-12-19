e28137e626a5a6bb3af3fc1378febe995d39699b
RafalSiuta <r85unit@gmail.com>
2024-12-19 21:21:29 +0100

    1.fix navigation active class error; 2.add smooth scrolling behavior; 3.fix z-index small device menu issue;

--- .history/active-nav_20241219195834.js
+++ .history/active-nav_20241219195834.js
@@ -1,4 +1,23 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//                 // Pobierz wszystkie linki w nawigacji
+//                 const navLinks = document.querySelectorAll('nav a');
 
+//                 // Funkcja dodająca klasę 'active' do klikniętego linku i usuwająca ją z innych
+//                 function setActiveClass(event) {
+//                     // Usuń klasę 'active' ze wszystkich linków
+//                     navLinks.forEach(link => {
+//                         link.classList.remove('active');
+//                     });
+
+//                     // Dodaj klasę 'active' do klikniętego linku
+//                     event.currentTarget.classList.add('active');
+//                 }
+
+//                 // Dodaj nasłuchiwacz zdarzeń do każdego linku
+//                 navLinks.forEach(link => {
+//                     link.addEventListener('click', setActiveClass);
+//                 });
+//             });
 
 document.addEventListener("DOMContentLoaded", function () {
     // Pobierz wszystkie linki i sekcje
